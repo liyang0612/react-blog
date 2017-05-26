615e6bd90dff40e9c35f88d8026e4a435255196d
356514033 <liyang0612@github.com>
2017-05-26 22:15:56 +0800

    页面风格,添加页面组件

--- myMessage/component/home/home.jsx
+++ myMessage/component/home/home.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router'
+import List from './list'
 class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				<div className="row home-banner">
-					<div className="navbar navbar-static-top bs-docs-nav">
-						<div className="container">
-							<a className="navbar-brand">About</a>
-							<Link to="/message" className="navbar-brand">Message</Link>
-						</div>
+				<header className="container">
+					<div className="text-center blog-nav">
+						<ul>
+							<li>
+								<a>About</a>
+							</li>
+							<li>
+								<Link to="/message">Message</Link>
+							</li>
+						</ul>
 					</div>
-					<div className="col-md-12"></div>
-				</div>
+				</header>
+				<section className="container bs-docs-container">
+					<List/>
+				</section>
 			</div>
 		)
 	}
---
+++ myMessage/component/home/list.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router'
+class List extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="bs-docs-section">
+                        <div className="blog-list-box">
+                            <h3>这是文章列表标题</h3>
+                            <p>这里是日期</p>
+                            <div className="blog-list-text">
+                                这里是部分文章内容
+                            </div>
+                            <Link>more...</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default List
